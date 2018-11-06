e8f8ca3a0a1e7e373234e5d5fd022a83feae457e
albertobazilio98 <albertobazilio98@gmail.com>
2018-11-06 15:30:54 -0300

    Controle sua mãe + 3 negão

---
+++ Java&Arduino/src/controle/BatataC.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controle;
+
+/**
+ *
+ * @author alber
+ */
+public class BatataC {
+    
+    public float cutPot (String ser) {
+        return Float.parseFloat(ser.substring(0, 3));
+    }
+    
+    public boolean cutLed (String ser) {
+        return Boolean.parseBoolean(ser.substring(4,4));
+    }
+    
+    public boolean cutBtn (String ser) {
+        return Boolean.parseBoolean(ser.substring(5,5));
+    }
+    
+}
